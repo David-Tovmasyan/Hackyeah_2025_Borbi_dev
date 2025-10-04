45faf2d75403d583b60436fbcf8a16a7fb5ab374
David Tovmasyan <dtovmasyan2022@gmail.com>
2025-10-04 23:13:32 +0200

    capacitor notifications

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,3 +1,12 @@
+<script>
+    import {goto} from "$app/navigation";
+    import {onMount} from "svelte";
+
+    onMount(()=>{
+        goto("/app")
+    })
+</script>
+
 <div class="text-4xl">
     Landing Page
 </div>
